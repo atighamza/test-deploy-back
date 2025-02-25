668a7edebf0fa462145cf1950fae00f2040a0997
hamza atig <hamzaatig2000@gmail.com>
2025-02-25 12:46:02 +0100

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
