ea6a926fe1d29a868cf9b15f34a4a1d72a883b81
hamza atig <hamzaatig2000@gmail.com>
2025-02-25 12:53:39 +0100

    names added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+const names = ["hamza", "atig", "dsfsqdsqd"];
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.json(names);
 });
 
 app.listen(PORT, () => {
